2542a7ed0e8a27361378e12ea2249b5fcf5e052d
gerg <gergely.zalai@arworks.com>
2021-12-15 18:08:06 +0100

    script to git repo:

---
+++ mersz_downloader.py
@@ -0,0 +1,54 @@
+import pdfkit
+from urllib.request import urlopen
+import argparse
+from PyPDF2 import PdfFileMerger
+
+parser = argparse.ArgumentParser(description='Optional app description')
+
+options = {
+	'page-size': 'A4',
+   	'enable-local-file-access': None
+}
+base_url = 'https://mersz.hu/'
+document_url = base_url +'/dokumentum/'
+pages = []
+
+parser.add_argument('--page_count', '-pc', type=int,
+                    help='Page count')
+parser.add_argument('--document_pre', '-dp', type=String,
+                    help='Document prefix: like dj77mgtan__')
+parser.add_argument('--pdf_name', '-pn', type=String,
+                    help='Name of the generated pdf document')
+
+args = parser.parse_args()
+
+page_count = args.page_count
+document = args.document_pre
+pdf_name = args.pdf_name
+
+for page_nr in range(1,page_count+1):
+	url = document_url+document_pre+str(page_nr)
+	html_text = urlopen(url).read().decode('utf-8')
+
+
+
+	html_text = str(html_text).replace('<header', '<header style="display:none" ') \
+	                     .replace('<footer', '<footer style="display:none" ') \
+	                     .replace('src="/assets/', 'src="'+base_url+'/assets/') \
+	                     .replace('href="/assets/', 'href="'+base_url+'/assets/') \
+	                     .replace('href="/mod/', 'href="' + base_url + '/mod/') \
+	                     .replace('src="/mod/', 'src="'+base_url+'/mod/')
+
+
+
+	tmp = './temp/'+str(page_nr)+'.pdf'
+	pdfkit.from_string(html_text, tmp, verbose=True, options = options)
+	pages.append(tmp)
+
+
+# merge PDFS
+
+merger = PdfFileMerger()
+[merger.append(pdf,import_bookmarks=False) for pdf in pages]
+with open(pdf_name, "wb") as new_file:
+	merger.write(new_file)
